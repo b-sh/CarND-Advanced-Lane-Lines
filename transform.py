6d0ca8f3bbf906c5551f8395f2ba082ebb6e7f3e
b-sh <b-sh@gmx.net>
2017-02-25 22:44:48 +0100

    binary transform

--- transform.py
+++ transform.py
@@ -5,70 +5,30 @@ import matplotlib.pyplot as plt
 import pickle
 
 # pipline output images
-path        = "output_images/"
-path_calib  = "camera_cal/" 
-shape       = None
+path_image  = "test_images/"
+path_out    = "output_images/"
 
-nx = 9
-ny = 6
-
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 5, 0.001)
- 
-# prepare object points
-# creating 3D object points and mapping to 2D
-# http://docs.opencv.org/3.1.0/dc/dbb/tutorial_py_calibration.html
-objp = np.zeros((nx*ny,3), np.float32)
-objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
-
-def get_corners(img_gray):
-    # Find the chessboard corners
-    ret, corners = cv2.findChessboardCorners(img_gray, (nx, ny), None)
-
-    # when not found just return None
-    if ret != True:
-        return None
-
-#    corners = cv2.cornerSubPix(img_gray,corners,(1,1),(-1,-1),criteria)
-    return corners
-
-objpoints = []
-imgpoints = []
-for image in os.listdir(path_calib):
-    img            = cv2.imread(path_calib+image)
-    # Convert to grayscale
-    gray           = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgpoints_temp = get_corners(img)
-
-    if imgpoints_temp is not None:
-        objpoints.append(objp)
-        imgpoints.append(imgpoints_temp)
-
-        img  = cv2.drawChessboardCorners(img, (nx, ny), imgpoints_temp, True)
-        cv2.imwrite(path+"corners"+image, img)
+def abs_sobel_thresh(img_gray, orient='x', thresh_min=0, thresh_max=255):    
+    if orient == 'x':
+        sobel       = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0)
     else:
-        print(image)
+        sobel       = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1)
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+    abs_sobel    = np.absolute(sobel)
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
 
-calib = {}
-calib['mtx'] = mtx
-calib['dist'] = dist
+    sbinary = np.zeros_like(scaled_sobel)
+    sbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
 
-pickle.dump(calib, open("calibration.p","wb"))
+    return sbinary
 
-for image in os.listdir(path_calib):
-    img          = cv2.imread(path_calib+image)
+for image in os.listdir(path_image):
+    img          = cv2.imread(path_image+image)
+    gray         = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(path_out+"grey"+image, gray)
 
-    undistorted = cv2.undistort(img, mtx, dist, None, mtx)
-    cv2.imwrite(path+"undistorted"+image, undistorted)
+    grad_binary = abs_sobel_thresh(gray, orient='x', thresh_min=0, thresh_max=255)
+    cv2.imwrite(path_out+"binary_sobelx"+image, grad_binary)
 
-# http://docs.opencv.org/3.1.0/dc/dbb/tutorial_py_calibration.html
-total_error = 0
-for i in range(len(objpoints)):
-     imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-     error         = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-     total_error    += error
- 
-print("total error: ", total_error/len(objpoints))
-
+    grad_binary = abs_sobel_thresh(gray, orient='y', thresh_min=0, thresh_max=255)
+    cv2.imwrite(path_out+"binary_sobely"+image, grad_binary)
